ea77d9f60cc1fe8756284d45167277eda2350808
ユーザー名 <>
2024-04-30 06:20:50 +0900

    R60430

--- cho_matey/cho_matey/urls.py
+++ cho_matey/cho_matey/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('choapp/',include('choapp.urls')),
+    path('', include('choapp.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
